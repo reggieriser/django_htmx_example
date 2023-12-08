f567980a5da2616c8c63d70448906198927879f3
Reggie Riser <reggie@truss.works>
2023-12-08 12:56:57 -0500

    Fixed off by one bug; made max results a constant

--- django_htmx_example/locations/views.py
+++ django_htmx_example/locations/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 
 from .models import DutyLocation
 
+MAX_RESULTS = 50
+
 
 def search(request):
     search_term = request.GET.get("search", "")
     has_more = False
     if search_term:
-        duty_locations = DutyLocation.objects.filter(name__icontains=search_term).order_by("name")[:51]
-        if len(duty_locations) > 50:
+        duty_locations = DutyLocation.objects.filter(name__icontains=search_term).order_by("name")[: MAX_RESULTS + 1]
+        if len(duty_locations) > MAX_RESULTS:
+            duty_locations = duty_locations[:MAX_RESULTS]
             has_more = True
     else:
         duty_locations = None
@@ -16,4 +19,8 @@ def search(request):
         template_to_render = "locations/partials/results.html"
     else:
         template_to_render = "locations/location_search.html"
-    return render(request, template_to_render, {"duty_locations": duty_locations, "has_more": has_more})
+    return render(
+        request,
+        template_to_render,
+        {"duty_locations": duty_locations, "has_more": has_more, "max_results": MAX_RESULTS},
+    )
